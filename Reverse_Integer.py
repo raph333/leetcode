9b8a76b999be0f1734b33596086b97c2597a920c
Raphael Peer <raphaelpeer@hotmail.com>
2019-11-24 15:46:04 +0100

    reverse integer: no-string method

--- Reverse_Integer.py
+++ Reverse_Integer.py
@@ -1,21 +1,36 @@
 
 class Solution:
-    """
-    cases to cover:
-    * sign
-    * trailing zeros (covered if str is converted to int)
-    """
 
-    def reverse(self, x: int) -> int:
+    def reverse1(self, x: int) -> int:
         """
         Trivial approach: convert to string
         """
         s = str(abs(x))
         rev = int(s[::-1])
 
-        if rev >= 2**31:
+        if rev >= 2 ** 31:
             return 0
         else:
             sign = -1 if x < 0 else 1
             return rev * sign
+
+    def reverse(self, x: int) -> int:
+        """
+        Integer-only version
+        """
+        sign = -1 if x < 0 else 1
+        num = abs(x)
+        rev = 0
+
+        while num > 0:
+            trailing_digit = num % 10
+            num //= 10
+
+            rev *= 10
+            rev += trailing_digit
+
+            if rev >= 2 ** 31:
+                return 0
+
+        return rev * sign
 
